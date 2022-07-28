1c04773d39d58f1f3015d5c08f27e986eda0b7ba
N7 GIL <gilbertoaescobedo@gmail.com>
2022-07-27 18:33:20 -0700

    Was able to properly install inquirer. Started getting functional prompts in the terminal. Wrote prompts

--- index.js
+++ index.js
@@ -1,41 +1,93 @@
-const inquirer = require('inquirer');
+import inquirer from 'inquirer';
+// const fs = require('fs');
+import * as fs from 'fs';
+
+// let fileContent = "";
+
+
 
 inquirer
   .prompt([
     {
       type: 'input',
-      message: 'What are the names of your team members?',
-      name: 'membernames',
+      name: 'manager_name',
+      message: "What's your manager's name?",
     },
     {
       type: 'input',
-      message: 'What is your email?',
-      name: 'email',
+      name: 'manager_id',
+      message: "What's your manager's id?",
     },
     {
       type: 'input',
-      message: 'What is your Github?',
-      name: 'github',
+      name: 'manager_email',
+      message: "What's your manager's email?",
     },
+    {
+      type: 'input',
+      name: 'manager_phone',
+      message: "What's your manager's office number?",
+    },
+    {
+      type: 'list',
+      name: 'engineer_intern',
+      message: "Do you have an engineer or intern?",
+      choices: ['engineer','intern']
+    },
+
+    {
+      type: 'input',
+      name: 'engineer_name',
+      message: "What's your engineer's name?",
+    },
+    {
+      type: 'input',
+      name: 'engineer_id',
+      message: "What's your engineer's id?",
+    },
+    {
+      type: 'input',
+      name: 'engineer_email',
+      message: "What's your engineer's email?",
+    },
+    {
+      type: 'input',
+      name: 'engineer_phone',
+      message: "What's your engineer's office phone number?",
+    },
+    {
+      type: 'input',
+      name: 'intern_name',
+      message: "What's your intern's name?",
+    },
+    {
+      type: 'input',
+      name: 'intern_id',
+      message: "What's your intern's id?",
+    },
+    {
+      type: 'input',
+      name: 'intern_email',
+      message: "What's your intern's email?",
+    },
+    {
+      type: 'input',
+      name: 'intern_phone',
+      message: "What's your intern's office phone number?",
+    },
+    
+
 
   ])
-  .then((response) =>
-    response.confirm === response.membernames
-      ? console.log('Got those names!')
-      : console.log('I did not get that')
+.then((response) => {
+
+  // fs.writeFile('index.html', response.manager_name,)
+
+  console.log(response.manager_name);
+  console.log(response.manager_id);
+    
       
-  );
+});
 
-  then((response) =>
-      response.confirm === response.email
-          ? console.log('Nice email!')
-          : console.log('Try again')
-
-  );
-
-  then((response) =>
-      response.confirm === response.github
-          ? console.log('Awesome!')
-          : console.log('Negative')
-  );
+// writeFile('index.html', response.first_name);
 
